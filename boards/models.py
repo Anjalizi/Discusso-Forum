1f1646b23a1d6069a7a1b6648fa1ea3d08ef4282
Anjali Jain <aj2966@gmail.com>
2017-12-17 14:23:02 +0530

    Made homepage and authentication pages

---
+++ boards/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.contrib.auth.models import User
+#there is an in-built user model
+
+class Board(models.Model):
+    Name = models.CharField(max_length=30, unique=True)
+    Description = models.CharField(max_length=100)
+
+    def __str__(self):
+    	return self.Name
+
+class Topic(models.Model):
+    subject = models.CharField(max_length=100)
+    last_updated = models.DateTimeField(auto_now_add=True)
+    board = models.ForeignKey(Board, related_name='Topics')
+    starter = models.ForeignKey(User, related_name='Topics') #reverse relationships exist by default, 'Topics' is the name given
+
+class Post(models.Model):
+    message = models.TextField(max_length=4000)
+    topic = models.ForeignKey(Topic, related_name='Posts')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(null=True, auto_now_add=True)
+    created_by = models.ForeignKey(User, related_name='Posts')
+    updated_by = models.ForeignKey(User, null=True, related_name='+') #related_name='+' means no reverse relationship
---
+++ myp/urls.py
@@ -0,0 +1,39 @@
+from django.conf.urls import url
+from django.contrib import admin
+from django.contrib.auth import views as auth_views
+
+from boards import views
+from accounts import views as accounts_views
+
+urlpatterns = [
+    url(r'^$', views.home, name='home'), #^$ denotes empty path i.e takes us to 127.0.0.1:8000
+    url(r'^signup/$', accounts_views.signup, name='signup'),
+    url(r'^login/$', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    url(r'^logout/$', auth_views.LogoutView.as_view(), name='logout'), #class-based views provide a more flexible way to extend and reuse views
+
+    url(r'^reset/$',
+        auth_views.PasswordResetView.as_view(
+            template_name='password_reset.html', #optional
+            email_template_name='password_reset_email.html',
+            subject_template_name='password_reset_subject.txt'
+        ),
+        name='password_reset'),
+    url(r'^reset/done/$',
+        auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'),
+        name='password_reset_done'),
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'),
+        name='password_reset_confirm'),
+    url(r'^reset/complete/$',
+        auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'),
+        name='password_reset_complete'),
+
+    url(r'^settings/password/$', auth_views.PasswordChangeView.as_view(template_name='password_change.html'),
+        name='password_change'),
+    url(r'^settings/password/done/$', auth_views.PasswordChangeDoneView.as_view(template_name='password_change_done.html'),
+        name='password_change_done'),
+
+    url(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
+    url(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
+    url(r'^admin/', admin.site.urls),
+]
