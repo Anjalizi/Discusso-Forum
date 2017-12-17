cbdf91d9ee2a8c82122e5f6ee8015e5ff79a3c5e
Anjali Jain <aj2966@gmail.com>
2017-12-17 16:09:46 +0530

    added user authentication with posts

--- boards/tests.py
+++ boards/tests.py
@@ -1,8 +1,10 @@
 from django.core.urlresolvers import reverse
 from django.test import TestCase
-from .views import home, board_topics
-from django.urls import resolve
-from .models import Board
+from .views import home, board_topics, topic_posts
+from django.urls import resolve, reverse
+from .models import Board, Topic, Post
+
+from django.contrib.auth.models import User
 
 class HomeTests(TestCase):
 	def setUp(self):
@@ -45,3 +47,19 @@ class BoardTopicsTests(TestCase):
 	def test_board_topics_url_resolves_board_topics_view(self):
 		view = resolve('/boards/1/')
 		self.assertEquals(view.func, board_topics)
+
+class TopicPostsTests(TestCase):
+    def setUp(self):
+        board = Board.objects.create(Name='Django', Description='Django board.')
+        user = User.objects.create_user(username='john', email='john@doe.com', password='123')
+        topic = Topic.objects.create(subject='Hello, world', board=board, starter=user)
+        Post.objects.create(message='Lorem ipsum dolor sit amet', topic=topic, created_by=user)
+        url = reverse('topic_posts', kwargs={'pk': board.pk, 'topic_pk': topic.pk})
+        self.response = self.client.get(url)
+
+    def test_status_code(self):
+        self.assertEquals(self.response.status_code, 200)
+
+    def test_view_function(self):
+        view = resolve('/boards/1/topics/1/')
+        self.assertEquals(view.func, topic_posts)
